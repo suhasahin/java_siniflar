a10146cce3950d3ea014d23d3745cfee3813ec5b
osman <suhasahin@gmail.com>
2023-01-19 00:43:54 +0300

    java siniflar

---
+++ src/Car.java
@@ -0,0 +1,42 @@
+import java.sql.SQLOutput;
+
+public class Car {
+    String type;
+    String model;
+    String color;
+    int speed;
+    int speedLimit = 180;
+
+    Car(String model, int speed, String color) {
+        this.model = model;
+        this.speed = speed;
+        this.color = color;
+        this.type = "Sedan";
+        this.speedLimit = 180;
+
+    }
+
+    void increaseSpeed(int increment){
+
+        if ((speed + increment) < speedLimit){
+            speed += increment;
+        }
+    }
+
+    void decreaseSpeed(int decrease){
+        if (speed > 0) {
+            speed -= decrease;
+        }
+    }
+
+    void printSpeed(){
+        System.out.println(model + " Hızınız : " + speed);
+    }
+
+    void printInfo(){
+        System.out.println("Model : \t"+ this.model);
+        System.out.println("Color : \t"+ this.color);
+        System.out.println("Type : \t"+ this.type);
+        System.out.println("Speed : \t"+ this.speed);
+    }
+}
